7ae5e8146251cc57224b85ef407b02c8744022d9
SweetoCodes <charliesweeting18@gmail.com>
2022-01-21 14:54:07 +0000

    added navbar icon fallback and extended navbar skeleton

--- components/atoms/icons/profile-picture-image.js
+++ components/atoms/icons/profile-picture-image.js
@@ -1,38 +1,17 @@
 import Image from "next/image";
 
-export default function ProfilePictureImage({currentUser, style}) {
-    return (     <div className={style}>
-              <Image
-                className="object-cover rounded-full transition duration-1000 ease-in-out"
-                src={currentUser.photoURL}
-                alt="me"
-                width="50"
-                height="50"
-                layout="responsive"
-                alt="Profile Picture"
-              />
-            </div>
-
-    );
-  }
-  
-
-// export default function ProfilePicture(props) {
-//   const { currentUser } = useContext(AuthContext);
-//   if (!currentUser) {
-//     return null;
-//   }
-//   return (
-//     <div className={props.style}>
-//       <Image
-//         className="object-cover rounded-full transition duration-1000 ease-in-out"
-//         src={currentUser.photoURL}
-//         alt="me"
-//         width="50"
-//         height="50"
-//         layout="responsive"
-//         alt="Profile Picture"
-//       />
-//     </div>
-//   );
-// }
+export default function ProfilePictureImage({ currentUser, style }) {
+  return (
+    <div className={style}>
+      <Image
+        className="object-cover rounded-full transition duration-1000 ease-in-out"
+        src={currentUser.photoURL}
+        alt="me"
+        width="50"
+        height="50"
+        layout="responsive"
+        alt="Profile Picture"
+      />
+    </div>
+  );
+}
--- components/molecules/navigation/default-navbar.js
+++ components/molecules/navigation/default-navbar.js
@@ -1,4 +1,6 @@
 import ProfilePictureImage from "../../atoms/icons/profile-picture-image";
+import ProfilePictureFallback from "../../atoms/icons/profile-picture-fallback";
+import Link from "next/link";
 
 export default function DefaultNavBar({
   currentUser,
@@ -9,15 +11,27 @@ export default function DefaultNavBar({
   return (
     <div
       className={
-        "flex flex-row max-w-7xl px-6 py-2 mx-auto justify-between border-b-2 transform duration-500 " +
+        "flex flex-row max-w-7xl px-6 py-2 mx-auto justify-between border-b-2 transform duration-500 items-center " +
         (scrolled ? " border-grey-100" : " border-white ")
       }
     >
-      <div className="uppercase font-bold">Motivez</div>
-      <div className="flex flex-row">
+      <Link href={currentUser ? "/home" : "/"}>
+        <div className="uppercase font-bold cursor-pointer">Motivez</div>
+      </Link>
+      <div className="sm:flex hidden flex-row items-center">
         <div>Page Nav</div>
-        <div>Login/Signup - User photo</div>
-        {currentUser ? <ProfilePictureImage currentUser={currentUser} style="" /> : <div></div> }
+        {currentUser ? (
+          currentUser.photoURL ? (
+            <ProfilePictureImage currentUser={currentUser} style="" />
+          ) : (
+            <ProfilePictureFallback style="" />
+          )
+        ) : (
+          <div>Login/Logout</div>
+        )}
+      </div>
+      <div className="sm:hidden flex">
+        {extendedNav ? <div>x</div> : <div>menu icon</div>}
       </div>
     </div>
   );
